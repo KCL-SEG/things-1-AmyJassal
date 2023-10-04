861c9b0b2a14ef7e0f4099cda8e090101d275e3f
Amy-Jassal <amy-jassal@hotmail.co.uk>
2023-10-04 01:36:37 +0100

    Change max length of name and description

--- things/models.py
+++ things/models.py
@@ -4,6 +4,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Thing (models.Model):
-    name = models.CharField(max_length=20, unique = True)
-    description = models.CharField(max_length=40, blank = True)
+    name = models.CharField(max_length=30, unique = True)
+    description = models.CharField(max_length=120, blank = True)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
